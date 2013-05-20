004611d8202a490df8fe655f782cfc415a3eb872
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-05-20 16:52:21 +0200

    FTP Deploy Test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>iColoris - Prototyp</title>
+	<meta charset="UTF-8" />
+	<meta name="description" content="Hier entsteht die Individuallösung für die iColoris GmbH" />
+	
+	<link href="style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+ <p>Die Lösung ist in Konzeption</p>
+</body>
+</html>
