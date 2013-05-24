173f5efde2429160269e0ae421b8ac7d2d674b6c
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-05-24 12:26:56 +0200

    asu FTP gezogen und .gitignore hinzugefügt

--- index.php
+++ index.php
@@ -1,13 +1,69 @@
-<!DOCTYPE html>
 <html>
 <head>
-	<title>iColoris - Prototyp</title>
-	<meta charset="UTF-8" />
-	<meta name="description" content="Hier entsteht die Individuallösung für die iColoris GmbH" />
-	
-	<link href="style.css" type="text/css" rel="stylesheet" />
+	<?php
+		include("nav.js");
+	?>
+	<link href="index.css" rel="stylesheet" type="text/css">
+	<link href="CRM/crm_style.css" rel="stylesheet" type="text/css">
+	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$( document ).ready(function() {
+			loadHomepage();
+		});
+	</script>
 </head>
 <body>
- <p>Die Lösung ist in Konzeption</p>
+
+<div class=container>
+	<div class=header>
+		<div class=logo>
+			
+		</div>
+		<div class=headerRight>
+			<div class=headerMenu>
+				<div class=headerMenuElement>
+					FAQ
+				</div>
+				<div class=headerMenuElement>
+					Kontakt
+				</div>
+					
+			</div>
+			<div class=headerAccount>
+				<ul>
+					<li>Login</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class=body>
+		<div class=hauptnav>
+			<div class=hauptNavElement>
+				Startseite
+			</div>
+			<div class=hauptNavElement>
+				<a href="#" onclick="loadCRMmain();">CRMmain</a>
+				<ul>
+				<li><a href="#" onclick="loadCRMtest();">CRMtest</a></li>
+				</ul>
+			</div>
+			<div class=hauptNavElement>
+				Kalender
+			</div>
+			<div class=hauptNavElement>
+				Posteingang
+			</div>
+		</div>
+		<div id="content" class=content>
+			Wird ersetzt.
+		</div>
+	</div>
+</div>
+
+<script language="JavaScript">
+	loadHomepage();
+</script>
+
 </body>
+
 </html>
