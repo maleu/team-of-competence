2159a202993fb5ebb730a5c52f1c26348eb894fd
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-05-26 12:38:48 +0200

    CRM Neukunde 0.6 SQL INSERT skizziert

--- CRM/crm_created.php
+++ CRM/crm_created.php
@@ -1,22 +1,52 @@
 <div id='crm_wrap'>
-	<?php 
-	$vorname = $_POST['P_vorname'];
-	$nachname = $_POST['P_nachname'];
-	echo 'TEST: '.$id; ?>
+	<?php
+	echo 'TEST: ' . $id;
+	?>
 	<div id='crmControls'>
-		<button class='crmNav' name="newClient" onclick="crm_createClient()" id="crm_newClient" value="new">Neuer Kunde</button>
-		<button name="findClient" class='crmNav' id='crm_findClient' value="search">Suche Kunde</button>
+		<button class='crmNav' name="newClient" onclick="crm_createClient()" id="crm_newClient" value="new">
+			Neuer Kunde
+		</button>
+		<button name="findClient" class='crmNav' id='crm_findClient' value="search">
+			Suche Kunde
+		</button>
 		<form>
 			<input name="crm_clientID" type="text" class='crmNav' id="crm_input_clientID" onkeydown="if (event.keyCode == 13) document.getElementById('crm_openByIDButton').click()" maxlength="5" />
-			<button type="button" id='crm_openByIDButton' onclick="crm_openClientbyID()" class='crmNav'>Aufrufen</button>
+			<button type="button" id='crm_openByIDButton' onclick="crm_openClientbyID()" class='crmNav'>
+				Aufrufen
+			</button>
 		</form>
-		<div id='clearfloat'> </div>
+		<div id='clearfloat'></div>
 	</div>
+
 	<?php
-	// IF POST!===NULL --> $id = $POST_id; eELSE id=0
-	//include_once 'crm_kundenprofil.php';
-	//include_once 'crm_kaufhistorie.php';
-	//include_once 'crm_kontaktverlauf.php';
-	echo('Neukunde '.$vorname.' '.$nachname.' wurde erfolgreich erstellt :)');
-	?>
+if (isset( $_POST['nachname'] ))
+{
+    if (get_magic_quotes_gpc()) { $_POST = array_map( 'stripslashes', $_POST ); }
+    $vorname    = $_POST['vorname'];
+    $nachname = $_POST['nachname'];
+ 
+	include_once 'db_connect.php';
+    if (mysqli_connect_errno() == 0)
+    {
+        $sql = 'INSERT INTO `Kunden` (`vorname`, `nachname`, `firma`) VALUES (?, ?, ?)';
+        $insert = $db->prepare( $sql );
+        $insert->bind_param( 'sss', $vorname, $nachname, $firma );
+        $insert->execute();
+
+        if ($eintrag->affected_rows == 1)
+        {
+            echo('Neukunde '.$vorname.' '.$nachname.' wurde erfolgreich erstellt :)');
+        }
+        else
+        {
+            echo 'Der Kunde konnte nicht erstellt werden...';
+        }
+    }
+    else
+    {
+        echo("<div id='dbcon'>No DB COnnection possible at all, error is ".mysqli_connect_errno()." : ".mysqli_connect_error()."</div><!--end dbcon -->"); }
+	//$db->close();
+    }
+ 
+?>
 </div>
