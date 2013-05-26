d4cb2352a8d28ddfabf058ee431d6bb61c239db2
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-05-26 12:16:38 +0200

    CRM Neukundenerstellung 0.5

---
+++ CRM/crm_neukunde.php
@@ -0,0 +1,13 @@
+<div id='kunde_neu'>
+	<form>
+		Vorname: <input name="crm_neuVorname" type="text" class='crmNeukundeForm' id="crm_input_Vorname" maxlength="20" />
+		Nachname: <input name="crm_neuNachname" type="text" class='crmNeukundeForm' id="crm_input_Nachname" maxlength="20" />
+		Firma: <input name="crm_neuFirma" type="text" class='crmNeukundeForm' id="crm_input_Firma" maxlength="20" />
+		Strasse & Hausnummer: <input name="crm_neuStrNr" type="text" class='crmNeukundeForm' id="crm_input_StrNr" maxlength="20" />
+		Postleitzahl: <input name="crm_neuPLZ" type="text" class='crmNeukundeForm' id="crm_input_PLZ" maxlength="20" />
+		Ort: <input name="crm_neuOrt" type="text" class='crmNeukundeForm' id="crm_input_Ort" maxlength="20" />
+		Telefonnummer: <input name="crm_neuTelefon" type="text" class='crmNeukundeForm' id="crm_input_Telefon" maxlength="20" />
+		eMail: <input name="crm_neueMail" type="text" class='crmNeukundeForm' id="crm_input_eMail" maxlength="20" />
+		<button type="button" id='crm_createClientButton' onclick="crm_createClientSubmit()" class='crmNeukundeForm'>Aufrufen</button>
+	</form>
+</div>
