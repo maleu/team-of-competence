3952c54a73b23de8b26f985d903d021f48bfea40
Martin Leuthold <martinleuthold-test@yahoo.de>
2013-05-28 15:37:06 +0200

    Weitere Oberflächen für das Modul FIBU hinzugefügt

---
+++ FIBU/lohnGehaltUeberweisen.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Description of lohnGehaltÜberweisen
+ *
+ * @author Martin Leuthold
+ */
+?>
+<h1>Lohn/Gehalt überweisen</h1>
+<p>Bitte geben Sie die Mitarbeiter ID oder den Namen des Mitarbeiter ein.</p>
+
+<table>
+    <tr>
+        <td>Mitarbeiter ID: <input type="text"/></td>
+        <td>Nachname: <input type="text"/></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td>Vorname: <input type="text"/></td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td>Kontoinhaber: (Nachname, Vorname)</td>
+        <td><input type="text"/></td>
+    </tr>
+    <tr>
+        <td>Bankleitzahl</td>
+        <td><input type="text"/></td>
+    </tr>
+    <tr>
+        <td>Kontonummer</td>
+        <td><input type="text"/></td>
+    </tr>
+</table>
+
+<textarea style="width: 100%;"></textarea>
+
+<button>Überweisen</button>
+
+<p>Hinweis: Hierdurch wird automatisch eine Meldung versandt!</p>
