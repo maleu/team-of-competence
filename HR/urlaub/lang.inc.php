197ae2dc653740146931900767cde2cc2f712ad4
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-06-05 19:52:56 +0200

    Urlaub die erste

---
+++ HR/urlaub/lang.inc.php
@@ -0,0 +1,7 @@
+<?php
+$lang = array();
+$lang['months'] = 'January,February,March,April,May,June,July,August,September,October,November,December'; 
+$lang['year'] = 'Year'; 
+$lang['states'] = 'RESERVED,OCCUPIED,FREE'; 
+$lang['type'] = 'lang'; 
+?>
---
+++ index2.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+</head>
+<body>
+		Benutzername: <input type="text" id="Benutzer Login"></input><br/>
+		Passwort:     <input type="text" id="Benutzer Passwort"></input><br/>
+		 <input type="submit" value="Login"></input>
+</body>
+</html>
