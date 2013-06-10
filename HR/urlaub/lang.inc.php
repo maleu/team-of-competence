b2893719653ed3e28d93f09b7792984b11d5fd52
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-06-10 18:23:18 +0200

    kontakt neu unfertig

--- HR/urlaub/lang.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$lang = array();
-$lang['months'] = 'January,February,March,April,May,June,July,August,September,October,November,December'; 
-$lang['year'] = 'Year'; 
-$lang['states'] = 'RESERVED,OCCUPIED,FREE'; 
-$lang['type'] = 'lang'; 
-?>
