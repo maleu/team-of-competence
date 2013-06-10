a087981faeeaabf4ad732a7be81fd0baa52be7b4
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-06-10 17:13:46 +0200

    Kaufhistorie klappt

--- CRM/crm_kaufhistorie.php
+++ CRM/crm_kaufhistorie.php
@@ -7,27 +7,28 @@ if (mysqli_connect_errno() ==0 )
 	<table id="table_kaufhistorie">
 		<tbody>
 			<tr class="toprow">
-				<td>KundenID</td>
 				<td>MitarbeiterID</td>
 				<td>ArtikelID</td>
 				<td>Verkaufsdatum</td>
-                                <td>Kanal</td>
+                <td>Kanal</td>
 			</tr>
-	<?php /* Ich habe soweit wie möglich den Statement ergänzt!!!
-$sql = "SELECT * FROM Verkaufsliste WHERE id=".$ArtikelID;
+	<?php 
+$sql = "SELECT KundenID, MitarbeiterID, Verkaufsdatum, Kanal, Name, Preis FROM Verkaufsliste AS Verkauf JOIN ArtikelTemp AS Artikel ON Verkauf.ArtikelID = Artikel.ArtikelID WHERE Verkauf.KundenID=".$id;
 $result=$db->prepare($sql);
 $result->execute();
-$result->bind_result($KundenID, $MitarbeiterID, $ArtikelID, $Verkaufsdatum, $Kanal);
-while ($result->fetch()){ */
+$result->bind_result($kundenID, $mitarbeiterID, $verkaufsdatum, $kanal, $name, $preis);
+while ($result->fetch()){
 			?>
 			<tr>
-				<td><?php echo '$KundenID' ?></td>
-				<td><?php echo '$MitarbeiterID' ?></td>
-				<td><?php echo '$ArtikelID' ?></td>
-				<td><?php echo '$Verkaufsdatum' ?></td>
-                                <td><?php echo '$Kanal' ?></td>
+				<td><?php echo $KundenID ?></td>
+				<td><?php echo $MitarbeiterID ?></td>
+				<td><?php echo $name ?></td>
+				<td><?php echo $preis ?></td>
+				<td><?php echo $Verkaufsdatum ?></td>
+                <td><?php echo $Kanal ?></td>
 			</tr>
-			<?php //} end while ?>
+			<?php } //end while 
+			?>
 		</tbody>
 	</table>
 </div>
