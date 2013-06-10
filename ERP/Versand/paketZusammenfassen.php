8aea8a90b9971bdfc911c78cebbe1721d56f6510
kmkkmk <punani2006>
2013-06-10 19:56:43 +0200

    versand

---
+++ ERP/Versand/paketZusammenfassen.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Description of wareingangsrechnungLieferantEditieren
+ *
+ * @author Martin Koltermann
+ */
+?>
+<h1>Paket zusammenfassen</h1>
+
+
+<table>
+    <tr>
+        <td>Identifikationsnummern:</td>
+        <td><input type="text"/></td>
+    </tr>
+    <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+     <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+     <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+     <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+     <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr> <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+     <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+     <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr> <tr>
+              <td></td>  <td><input type="text"/></td>
+    </tr>
+    
+</table>
+
+<p>
+    
+<a class="btn" href="javascript:$.growlUI('Paket erfolgreich zusammengestellt!');">Paket zusammenstellen</a>
