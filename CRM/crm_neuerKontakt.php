7b4ed7810ddb9f8be1dc26380f1e6564c6389db7
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-06-10 18:23:18 +0200

    kontakt neu unfertig

---
+++ CRM/crm_neuerKontakt.php
@@ -0,0 +1,10 @@
+<div id='kunde_neu'>
+	<form>
+		Kontaktart: <input name="P_kontaktart" type="text" class='crmNeukundeForm' id="crm_input_Kontaktart" maxlength="30" />
+		Inhalt: <input name="P_inhalt" type="text" class='crmNeukundeForm' id="crm_input_Inhalt" maxlength="20" />
+		Mitarbeiter: <input name="P_mitarbeiter_id" type="text" class='crmNeukundeForm' id="crm_input_MitarbeiterID" maxlength="20" />
+		<input name="P_datetime" type="" class='crmNeukundeForm' id="crm_input_Email" maxlength="20" />
+		MitarbeiterID: <input name="P_Mitarbeiter" type="text" class='crmNeukundeForm' id="crm_input_Mitarbeiter" maxlength="20" />
+		<button type="button" id='crm_createClientButton' onclick="crm_createClientSubmit()" class='crmNeukundeForm'>erstellen</button>
+	</form>
+</div>
--- HR/urlaub/conf.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conf = array();
-$conf['type'] = 'conf'; 
-$conf['login'] = 'user'; 
-$conf['password'] = 'test'; 
-$conf['copyright'] = 'Yes'; 
-?>
