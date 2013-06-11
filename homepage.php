f9dc8cf4b97ef93e14e3481e526f35ce80f8f61a
Tobias Ebbing <tobi@2Be-Ubuntu.(none)>
2013-06-11 17:59:46 +0200

    nav restore, Dashboard 2

--- homepage.php
+++ homepage.php
@@ -3,15 +3,15 @@ $mitarbeiter = $_POST['role'];
 echo 'Hallo ' . $mitarbeiter . ", wie immer begrüßt Dich Dein aufbereitetes Dashboard. Hab' einen schönen Tag!";
 ?>
 
-<div class="dashboard-div" id="dashboard-div-top-left">
+<div class="dashboard-div-left" id="dashboard-div-top-left">
 	TOP LEFT
 </div>
-<div class="dashboard-div" id="dashboard-div-top-right">
+<div class="dashboard-div-right" id="dashboard-div-top-right">
 	TOP RIGHT
 </div>
-<div class="dashboard-div" id="dashboard-div-row2-left">
+<div class="dashboard-div-left" id="dashboard-div-row2-left">
 	ROW2 LEFT
 </div>
-<div class="dashboard-div" id="dashboard-div-row2-right">
+<div class="dashboard-div-right" id="dashboard-div-row2-right">
 	ROW2 RIGHT
 </div>
