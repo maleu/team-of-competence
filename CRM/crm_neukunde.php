922b8bae3af7dacc127c9e5047b0076212b9bf19
Robert Kuls <robert.kuls@gmx.de>
2013-06-27 13:47:42 +0200

    main + crm

--- CRM/crm_neukunde.php
+++ CRM/crm_neukunde.php
@@ -1,14 +1,14 @@
 <div id='kunde_neu'>
 	<form>
-		Vorname: <input name="P_vorname" type="text" class='crmNeukundeForm' id="crm_input_Vorname" maxlength="20" />
-		Nachname: <input name="P_nachname" type="text" class='crmNeukundeForm' id="crm_input_Nachname" maxlength="20" />
-		Firma: <input name="P_firma" type="text" class='crmNeukundeForm' id="crm_input_Firma" maxlength="20" />
-		Strasse: <input name="P_Strasse" type="text" class='crmNeukundeForm' id="crm_input_Strasse" maxlength="20" />
-		Postleitzahl: <input name="P_PLZ" type="text" class='crmNeukundeForm' id="crm_input_PLZ" maxlength="20" />
-		Ort: <input name="P_Ort" type="text" class='crmNeukundeForm' id="crm_input_Ort" maxlength="20" />
-		Telefonnummer: <input name="P_Telefon" type="text" class='crmNeukundeForm' id="crm_input_Telefon" maxlength="20" />
-		Email: <input name="P_Email" type="text" class='crmNeukundeForm' id="crm_input_Email" maxlength="20" />
-		MitarbeiterID: <input name="P_Mitarbeiter" type="text" class='crmNeukundeForm' id="crm_input_Mitarbeiter" maxlength="20" />
+		Vorname: <input name="P_vorname" type="text" class='crmNeukundeForm' id="crm_input_Vorname" maxlength="20" /><br />
+		Nachname: <input name="P_nachname" type="text" class='crmNeukundeForm' id="crm_input_Nachname" maxlength="20" /><br />
+		Firma: <input name="P_firma" type="text" class='crmNeukundeForm' id="crm_input_Firma" maxlength="20" /><br />
+		Strasse: <input name="P_Strasse" type="text" class='crmNeukundeForm' id="crm_input_Strasse" maxlength="20" /><br />
+		Postleitzahl: <input name="P_PLZ" type="text" class='crmNeukundeForm' id="crm_input_PLZ" maxlength="20" /><br />
+		Ort: <input name="P_Ort" type="text" class='crmNeukundeForm' id="crm_input_Ort" maxlength="20" /><br />
+		Telefonnummer: <input name="P_Telefon" type="text" class='crmNeukundeForm' id="crm_input_Telefon" maxlength="20" /><br />
+		Email: <input name="P_Email" type="text" class='crmNeukundeForm' id="crm_input_Email" maxlength="20" /><br />
+		MitarbeiterID: <input name="P_Mitarbeiter" type="text" class='crmNeukundeForm' id="crm_input_Mitarbeiter" maxlength="20" /><br />
 		<id='crm_createClientButton' onclick="crm_createClientSubmit()" class='crmNeukundeForm'><a class="btn" href="javascript:$.growlUI('Meldung erfolgreich erstellt!');">erstellen</a>
 	</form>
 </div>
